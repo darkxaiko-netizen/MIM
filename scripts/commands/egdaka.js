6806bf91401621e02e00de32c482dfaf236c592b
『😽👉𝐑𝐉-𝐅𝐀𝐑𝐇𝐀𝐍👈😽』 <farhanbot220@gmail.com>
2025-08-07 07:06:29 +0600

    Update egdaka.js

--- scripts/commands/egdaka.js
+++ scripts/commands/egdaka.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 module.exports = {
   config:{
-  name: "@─꯭─⃝͎̽C⃪͓͢T⃔͢ B̷⃪⃨͢O⃔͢T̷⃪▁▁▁╱╱😈🙂🩵🪽",
+  name: "@কে'ট'বে'রি ত্যা'হ",
         version: "1.0.1",
         prefix: false,
   permssion: 0,
@@ -16,7 +16,7 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-  if (body.indexOf("oi")==0 || body.indexOf("oii")==0 || body.indexOf("@কে'ট'বে'রি ত্যা'হ")==0 || body.indexOf("@─꯭─⃝͎̽C⃪͓͢T⃔͢ B̷⃪⃨͢O⃔͢T̷⃪▁▁▁╱╱😈🙂🩵🪽")==0) {
+  if (body.indexOf("oi")==0 || body.indexOf("oii")==0 || body.indexOf("@কে'ট'বে'রি ত্যা'হ")==0 || body.indexOf("@everyone")==0) {
     var msg = {
         body: "~ ডাকো কেনো গো, আমি আছি তো এখানে..!✋🥀",
         attachment: fs.createReadStream(__dirname + `/Nayan/dakoknogo.mp3`)
