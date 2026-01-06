58d483e4f6f85c369a03f109591908da5ecfa98a
uffxan576-dotcom <uffxan576@gmail.com>
2026-01-06 16:17:19 +0600

    Update info..js

--- scripts/commands/info..js
+++ scripts/commands/info..js
@@ -27,13 +27,13 @@ const moment = require("moment-timezone");
 var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
 var link = ["https://i.postimg.cc/0NYJcjpR/Messenger-creation-1401370797218406.png", 
             
-            "https://i.postimg.cc/JhxzskNn/Snapchat-2014418092.jpg", 
+            "https://i.postimg.cc/XYVDwghc/IMG-20251225-060146-164.jpg", 
             
             "https://i.postimg.cc/0NYJcjpR/Messenger-creation-1401370797218406.png",
 
 "https://i.postimg.cc/8cJpXXqJ/Snapchat-670991066.jpg",
             
-            "https://i.postimg.cc/KvRXLsTB/FB-IMG-1709702111184.jpg"];
+            "https://i.postimg.cc/XYVDwghc/IMG-20251225-060146-164.jpg"];
   
 var callback = () => api.sendMessage({body:`🌺🍒🐰❀𝐀𝐃𝐌𝐈𝐍 𝐀𝐍𝐃 𝐁𝐎𝐓 𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐓𝐈𝐎𝐍💝🐰🍒 
 
@@ -67,7 +67,7 @@ _____________🅲🅾🅽🆃🅰🅲🆃_____________
 
 💥𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐈𝐃 (❶)💥 : https://www.facebook.com/profile.php?id=61550529403710
 
-💥𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐈𝐃 (❷)💥 : https://www.facebook.com/profile.php?id=61550121814016&mibextid=eBUYbo
+💥𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐈𝐃 (❷)💥 : https://www.facebook.com/DEVIL.FARHAN.420
 
 ✴️𝐁𝐎𝐓 𝐏𝐑𝐄𝐅𝐈𝐗✴️ : ${global.config.PREFIX}
 
